b18cecfb5b5b6c7fd3a8a8d3ec3ace8364166a5e
melody <877015767@qq.com>
2019-05-29 20:31:14 +0800

    test

---
+++ build/precache-manifest.269f806f402e975054ee2558e72fbcfc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c3c58b1e83ab8ccadb7c200ec2faaeb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8f246adad87d9dae4d39",
+    "url": "./static/css/2.ac93cb35.chunk.css"
+  },
+  {
+    "revision": "73118e688eeb7bbe4868",
+    "url": "./static/css/main.a3a59cd7.chunk.css"
+  },
+  {
+    "revision": "8f246adad87d9dae4d39",
+    "url": "./static/js/2.636de1ff.chunk.js"
+  },
+  {
+    "revision": "73118e688eeb7bbe4868",
+    "url": "./static/js/main.47eaa56e.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
